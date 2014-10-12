de722cc71a870a2fbc740179eafefd5bade9c765
Prophet731 <webmaster@amurray.me>
2014-10-12 05:55:33 -0700

    Added who's online

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -13,6 +13,20 @@ class BaseController extends Controller {
 		{
 			$this->layout = View::make($this->layout);
 		}
+
+		$this->whosOnline();
+	}
+
+	/**
+	 * Get's recent users online
+	 *
+	 * @return void
+	 */
+	protected function whosOnline()
+	{
+		$query = DB::select(File::get(storage_path() . '/sql/whos_online.sql'));
+
+		Config::set('webadmin.WHOSONLINE', $query);
 	}
 
 }
