34ecf55715ed48829789d579e4ed724cf843776d
Prophet731 <webmaster@amurray.me>
2014-10-27 05:11:46 -0700

    Fixed missing column in insert statement

--- app/storage/sql/upgrades/1.5.0beta1_2.0.0RC1.sql
+++ app/storage/sql/upgrades/1.5.0beta1_2.0.0RC1.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `tbl_chatlog` ADD INDEX (logDate);
 ALTER TABLE `bfadmincp_settings`
 CHANGE COLUMN `context` `context` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL;
-INSERT INTO `bfadmincp_settings` (`token`, `context`) VALUES ('CLANNAME', '');
+INSERT INTO `bfadmincp_settings` (`token`, `context`, `description`) VALUES ('CLANNAME', '', 'Set your clan or community name to be appened to the page title.');
 UPDATE `bfadmincp_settings` SET `context`='2.0.0-rc.1' WHERE `token` = 'VERSION';
