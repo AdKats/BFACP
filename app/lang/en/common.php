143c9b539485216c320a5bc19ed45daf6341b08e
Prophet731 <webmaster@amurray.me>
2015-02-09 12:34:06 -0800

    Added lang files

---
+++ app/lang/en/common.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'no_ban'         => 'No ban on file',
+    'no_infractions' => 'No infractions on file',
+    'ban_disabled'   => 'Ban Disabled',
+    'ban_permanent'  => 'Permanently Banned',
+    'ban_expired'    => 'Ban expired',
+    'ban_expire_in'  => 'Expires',
+
+    'login' => 'Login',
+    'logout' => 'Logout',
+
+    'nav' => [
+        'extras' => [
+            'psearch' => [
+                'placeholder' => 'Search for player...'
+            ]
+        ]
+    ]
+
+];
---
+++ app/lang/en/tables.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'playerlist' => [
+        'columns' => [
+            'col1' => 'ID',
+            'col2' => 'Game',
+            'col3' => 'Rank',
+            'col4' => 'Player',
+            'col5' => 'Ban',
+            'col6' => 'Reputation',
+            'col7' => 'Infractions',
+            'col8' => 'Country'
+        ],
+        'button_next' => 'Next',
+        'button_previous' => 'Previous',
+        'button_reset' => 'Reset'
+    ]
+
+];
