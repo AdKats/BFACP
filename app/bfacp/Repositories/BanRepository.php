eaf7cf09b605ab229875680fdf37541cf4fb4fdd
Prophet731 <webmaster@amurray.me>
2015-03-02 20:15:28 -0800

    Updated

--- app/bfacp/Repositories/BanRepository.php
+++ app/bfacp/Repositories/BanRepository.php
@@ -8,30 +8,30 @@ use Illuminate\Support\Facades\Cache;
 
 class BanRepository extends BaseRepository
 {
-	/**
-	 * Gets the latest bans
-	 * @param  string  $cacheKey Caching key to use
-	 * @param  integer $ttl      Cache for X minutes
-	 * @return array
-	 */
-	public function getLatestBans($cacheKey = 'bans.latest', $ttl = 1)
-	{
-		$bans = Cache::remember($cacheKey, $ttl, function() {
-			return Ban::with('player', 'record')->latest(30)->get()->toArray();
-		});
+    /**
+     * Gets the latest bans
+     * @param  string  $cacheKey Caching key to use
+     * @param  integer $ttl      Cache for X minutes
+     * @return array
+     */
+    public function getLatestBans($cacheKey = 'bans.latest', $ttl = 1)
+    {
+        $bans = Cache::remember($cacheKey, $ttl, function() {
+            return Ban::with('player', 'record')->latest(30)->get()->toArray();
+        });
 
-		return $bans;
-	}
+        return $bans;
+    }
 
-	/**
-	 * Gets the bans of the users soldiers
-	 * @param  array $ids Player IDs
-	 * @return array
-	 */
-	public function getPersonalBans($ids = [])
-	{
-		$bans = Ban::with('player', 'record')->personal($ids)->get()->toArray();
+    /**
+     * Gets the bans of the users soldiers
+     * @param  array $ids Player IDs
+     * @return array
+     */
+    public function getPersonalBans($ids = [])
+    {
+        $bans = Ban::with('player', 'record')->personal($ids)->get()->toArray();
 
-		return $bans;
-	}
+        return $bans;
+    }
 }
