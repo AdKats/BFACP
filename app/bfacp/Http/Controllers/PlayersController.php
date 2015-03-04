2af87131ddcd95dd59015c81eacb89f79c450f61
Prophet731 <webmaster@amurray.me>
2015-03-04 12:30:09 -0800

    Added player profile route

--- app/bfacp/Http/Controllers/PlayersController.php
+++ app/bfacp/Http/Controllers/PlayersController.php
@@ -1,8 +1,12 @@
 <?php namespace BFACP\Http\Controllers;
 
+use BFACP\Exceptions\PlayerNotFoundException;
 use BFACP\Repositories\PlayerRepository;
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\View;
+use MainHelper;
+
 class PlayersController extends BaseController
 {
     private $repository;
@@ -18,5 +22,25 @@ class PlayersController extends BaseController
         $page_title = Input::has('player') ? 'Player Search' : 'Player Listing';
 
         return View::make('player.listing', compact('page_title'));
+    }
+
+    public function profile($id, $name = '')
+    {
+        // Check if we have a cached version of the player
+        $isCached = Cache::has(sprintf('players.%s', $id));
+
+        // Cache for 10 minutes and get the player
+        $player = Cache::remember(sprintf('players.%s', $id), 10, function() use($id) {
+            return $this->repository->setopts([
+                    'bans',
+                    'reputation',
+                    'infractions'
+                ])->getPlayerById($id);
+        });
+
+        $page_title = ! empty($player->ClanTag) ?
+            sprintf('[%s] %s', $player->ClanTag, $player->SoldierName) : $player->SoldierName;
+
+        return View::make('player.profile', compact('player', 'isCached', 'page_title'));
     }
 }
