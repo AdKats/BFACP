c970db26fadacee59a6052dc498c749c73b96722
Dr_F1ST <info@bp4u.net>
2015-03-09 04:58:34 +0100

    Added German language

---
+++ app/lang/de/common.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'no_ban'         => 'Kein Bann vorhanden',
+    'no_infractions' => 'Keine Verstöße vorhanden',
+    'ban_disabled'   => 'Bann deaktiviert',
+    'ban_permanent'  => 'Permanent gebannt',
+    'ban_expired'    => 'Bann abgelaufen',
+    'ban_expire_in'  => 'Läuft ab',
+
+    'login' => 'Login',
+    'logout' => 'Logout',
+
+    'nav' => [
+        'extras' => [
+            'psearch' => [
+                'placeholder' => 'Suche nach dem Spieler...'
+            ]
+        ]
+    ]
+
+];
---
+++ app/lang/de/dashboard.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    'metro' => [
+        'players_online' => 'Spieler Online',
+        'average_bans' => 'Durchschnittliche Banns pro Tag',
+        'yesterday_bans' => 'Banns gestern',
+        'player_count' => 'Spezifische Spieler',
+        'adkats' => [
+            'titles' => [
+                'killed' => 'Getötete Spieler',
+                'kicked' => 'Gekickte Spieler',
+                'banned' => 'Gebannte Spieler',
+                'banned_active' => 'Aktive Banns'
+            ],
+            'killed' => ':%killed der Spieler wurden getötet',
+            'kicked' => ':%kicked der Spieler wurden gekickt',
+            'banned' => ':%banned der Spieler wurden gebannt',
+            'banned_active' => ':%banned der Spieler sind gebannt'
+        ]
+    ],
+
+    'population' => [
+        'title'  => 'Population',
+        'footer' => 'Gesamt',
+        'columns' => [
+            'col1' => 'Server',
+            'col2' => 'Online',
+            'col3' => 'Karte'
+        ]
+    ],
+
+    'bans' => [
+        'title' => 'Neuste Banns',
+        'columns' => [
+            'col1' => 'Spieler',
+            'col2' => 'Admin',
+            'col3' => 'ausgeführt',
+            'col4' => 'läuft ab'
+        ]
+    ],
+
+    'players_seen_country_past_day' => 'Spieler nach Land (24h)'
+];
