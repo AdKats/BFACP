e8755dd1e6c47adffc7ed4529973d00bd1f40f29
Prophet731 <webmaster@amurray.me>
2015-04-04 22:55:11 -0700

    Added missing use for HTML

--- app/bfacp/Http/Controllers/BaseController.php
+++ app/bfacp/Http/Controllers/BaseController.php
@@ -1,7 +1,7 @@
 <?php namespace BFACP\Http\Controllers;
 
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\HTML;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\View;
 use Lavary\Menu\Facade AS Menu;
@@ -13,18 +13,17 @@ class BaseController extends Controller
 
     public function __construct()
     {
-        $this->user = \App::make('bfadmincp')->user;
+        $this->user       = \App::make('bfadmincp')->user;
         $this->isLoggedIn = \App::make('bfadmincp')->isLoggedIn;
 
-        Menu::make('MainNav', function($menu)
-        {
+        Menu::make('MainNav', function ($menu) {
             $menu->raw(strtoupper(Lang::get('navigation.main.title')), ['class' => 'header']);
             $menu->add(Lang::get('navigation.main.items.dashboard'), ['route' => 'home'])->prepend(HTML::ficon('fa-dashboard'));
 
-            if(!$this->isLoggedIn) {
+            if (!$this->isLoggedIn) {
                 $menu->raw(strtoupper(Lang::get('navigation.admin.title')), ['class' => 'header']);
 
-                if(Entrust::can('admin.adkats.bans.view')) {
+                if (Entrust::can('admin.adkats.bans.view')) {
                     $menu->add(Lang::get('navigation.main.items.banlist'), ['route' => 'admin.adkats.bans.index'])->prepend(HTML::ficon('fa-hammer'));
                 }
             }
@@ -38,8 +37,7 @@ class BaseController extends Controller
      */
     protected function setupLayout()
     {
-        if ( ! is_null($this->layout))
-        {
+        if (!is_null($this->layout)) {
             $this->layout = View::make($this->layout);
         }
     }
