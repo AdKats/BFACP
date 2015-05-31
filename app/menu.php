717cd2006efc09507a0c06d37ef8a2c9913897b1
Prophet731 <webmaster@amurray.me>
2015-05-30 20:08:29 -0700

    Updated Menu

--- app/menu.php
+++ app/menu.php
@@ -13,19 +13,27 @@ $adminPermsList = Cache::remember('admin.perm.list', 60 * 24, function () {
     return $temp;
 });
 
-Menu::make('MainNav', function ($menu) use($adminPermsList) {
+Menu::make('MainNav', function ($menu) use ($adminPermsList) {
     $menu->raw(strtoupper(Lang::get('navigation.main.title')), ['class' => 'header']);
+
     $menu->add(Lang::get('navigation.main.items.dashboard.title'), ['route' => 'home'])
-         ->prepend(HTML::faicon(Lang::get('navigation.main.items.dashboard.icon.fa'), true));
+    ->prepend(HTML::faicon(Lang::get('navigation.main.items.dashboard.icon.fa'), true));
+
     $menu->add(Lang::get('navigation.main.items.scoreboard.title'), ['route' => 'servers.live'])
-         ->prepend(HTML::faicon(Lang::get('navigation.main.items.scoreboard.icon.fa'), true));
+    ->prepend(HTML::faicon(Lang::get('navigation.main.items.scoreboard.icon.fa'), true));
+
+    if (Auth::user()->ability(null, 'admin.adkats.bans.view')) {
+        $menu->add(Lang::get('navigation.admin.adkats.items.banlist.title'), ['route' => 'admin.adkats.bans.index'])
+        ->prepend(HTML::ionicon(Lang::get('navigation.admin.adkats.items.banlist.icon.ion'), true));
+    }
+
     $menu->add(Lang::get('navigation.main.items.playerlist.title'), ['route' => 'player.listing'])
-         ->prepend(HTML::faicon(Lang::get('navigation.main.items.playerlist.icon.fa'), true));
+    ->prepend(HTML::faicon(Lang::get('navigation.main.items.playerlist.icon.fa'), true));
 
     // If the role can access the chatlogs we can add the item to the navigation list
     if ((Auth::check() && Auth::user()->ability(null, 'chatlogs')) || Config::get('bfacp.site.chatlogs.guest')) {
         $menu->add(Lang::get('navigation.main.items.chatlogs.title'), ['route' => 'chatlog.search'])
-             ->prepend(HTML::faicon(Lang::get('navigation.main.items.chatlogs.icon.fa'), true));
+        ->prepend(HTML::faicon(Lang::get('navigation.main.items.chatlogs.icon.fa'), true));
     }
 
     // Only show these if the user is logged in
@@ -38,14 +46,9 @@ Menu::make('MainNav', function ($menu) use($adminPermsList) {
         if (Auth::user()->ability(null, $adminPermsList['adkats'])) {
             $adkats = $menu->raw(Lang::get('navigation.admin.adkats.title'));
 
-            if (Auth::user()->ability(null, 'admin.adkats.bans.view')) {
-                $adkats->add(Lang::get('navigation.admin.adkats.items.banlist.title'), ['route' => 'admin.adkats.bans.index'])
-                       ->prepend(HTML::ionicon(Lang::get('navigation.admin.adkats.items.banlist.icon.ion'), true));
-            }
-
             if (Auth::user()->ability(null, 'admin.adkats.user.view')) {
                 $adkats->add(Lang::get('navigation.admin.adkats.items.users.title'), ['route' => 'admin.adkats.users.index'])
-                       ->prepend(HTML::faicon(Lang::get('navigation.admin.adkats.items.users.icon.fa'), true));
+                ->prepend(HTML::faicon(Lang::get('navigation.admin.adkats.items.users.icon.fa'), true));
             }
         }
 
@@ -56,9 +59,9 @@ Menu::make('MainNav', function ($menu) use($adminPermsList) {
         if (Auth::user()->ability(null, $adminPermsList['site'])) {
             $site = $menu->raw(Lang::get('navigation.admin.site.title'));
 
-            if(Auth::user()->ability(null, 'admin.site.users')) {
+            if (Auth::user()->ability(null, 'admin.site.users')) {
                 $site->add(Lang::get('navigation.admin.site.items.users.title'), ['route' => 'admin.site.users.index'])
-                     ->prepend(HTML::faicon(Lang::get('navigation.admin.site.items.users.icon.fa'), true));
+                ->prepend(HTML::faicon(Lang::get('navigation.admin.site.items.users.icon.fa'), true));
             }
         }
     }
