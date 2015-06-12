bf765281f2efaef61d0bc2aae8c530e34d249e39
Prophet731 <webmaster@amurray.me>
2015-06-12 07:37:31 -0700

    Added updater section

---
+++ app/bfacp/Http/Controllers/Admin/UpdaterController.php
@@ -0,0 +1,39 @@
+<?php namespace BFACP\Http\Controllers\Admin;
+
+use BFACP\Http\Controllers\BaseController;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\View;
+use vierbergenlars\SemVer\version;
+
+class UpdaterController extends BaseController
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->guzzle = \App::make('GuzzleHttp\Client');
+    }
+
+    public function index()
+    {
+        $page_title = 'BFAdminCP Updater';
+
+        $latest_release = Cache::remember('latest_release', 24 * 60, function () {
+            $response = $this->guzzle->get('https://api.github.com/repos/Prophet731/BFAdminCP/releases/latest');
+            $latest_release = $response->json();
+
+            return $latest_release;
+        });
+
+        $releases = Cache::remember('releases', 24 * 60, function () {
+            $response = $this->guzzle->get('https://api.github.com/repos/Prophet731/BFAdminCP/releases');
+            $releases = $response->json();
+
+            return $releases;
+        });
+
+        $outofdate = version::lt(BFACP_VERSION, $latest_release['tag_name']);
+
+        return View::make('system.updater.index', compact('page_title', 'releases', 'outofdate', 'latest_release'));
+    }
+}
---
+++ app/views/system/updater/index.blade.php
@@ -0,0 +1,80 @@
+@extends('layout.main')
+
+@section('content')
+
+<div class="row">
+    <div class="col-xs-12">
+
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h4 class="box-title">Latest release</h4>
+                <div class="pull-right">
+                    @if($outofdate)
+                    <small class="label label-danger">Out of Date!</small>
+                    @else
+                    <small class="label label-success">You have the latest version</small>
+                    @endif
+                </div>
+            </div>
+
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-xs-12">
+                        @if(!empty($latest_release['body']))
+                        <p class="lead">{{ $latest_release['body'] }}</p>
+                        @endif
+                        <p>
+                            <a href="{{ $latest_release['zipball_url'] }}" class="btn btn-primary">
+                                <i class="fa fa-file-archive-o"></i>
+                                Download Zip
+                            </a>
+
+                            <a href="{{ $latest_release['tarball_url'] }}" class="btn btn-primary">
+                                <i class="fa fa-file-archive-o"></i>
+                                Download Gzip
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12">
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h2 class="box-title">Release History</h2>
+            </div>
+
+            <div class="box-body">
+                @foreach($releases as $key => $release)
+                <h2>{{ link_to($release['html_url'], $release['name'], ['target' => '_blank']) }} - <small ng-bind="moment('{{ $release['created_at'] }}').fromNow()"></small></h2>
+
+                @if($release['prerelease'])
+                <label class="label label-warning">Pre-Release</label>
+                @endif
+
+                @if(!empty($release['body']))
+                <p class="lead">{{ $release['body'] }}</p>
+                @endif
+
+                <p>
+                    <a href="{{ $release['zipball_url'] }}" class="btn btn-primary">
+                        <i class="fa fa-file-archive-o"></i>
+                        Download Zip
+                    </a>
+
+                    <a href="{{ $release['tarball_url'] }}" class="btn btn-primary">
+                        <i class="fa fa-file-archive-o"></i>
+                        Download Gzip
+                    </a>
+                </p>
+
+                @endforeach
+            </div>
+
+        </div>
+    </div>
+</div>
+
+@stop
