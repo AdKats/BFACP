7ae176a35c02897d63a261c5c6fb91dc5e12f301
Prophet731 <webmaster@amurray.me>
2015-06-15 14:09:07 -0700

    Created alerts language file

---
+++ app/lang/en/alerts.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'user' => [
+        'soldier_taken' => 'Player #:playerid has already been taken by another user.',
+        'invalid'       => 'User #:userid does not exist.',
+        'deleted'       => ':username has been deleted.'
+    ]
+
+];
