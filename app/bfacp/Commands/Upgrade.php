87e26adcd9835146a7070cbdbbc2d354a8ef1b18
Prophet731 <webmaster@amurray.me>
2015-06-15 18:45:14 -0700

    Created upgrade script

---
+++ app/bfacp/Commands/Upgrade.php
@@ -0,0 +1,42 @@
+<?php namespace BFACP\Commands;
+
+use Illuminate\Console\Command;
+
+class Upgrade extends Command
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'bfacp:upgrade';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Upgrade the BFACP to the latest version';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        //
+    }
+
+}
