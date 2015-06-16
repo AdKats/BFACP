a4c3ceadc08323fac91d6c152ac8efc786a52256
Prophet731 <webmaster@amurray.me>
2015-06-15 18:47:39 -0700

    Dynamically resolve BFACP commands

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -9,5 +9,11 @@
 | that it is available to be called. We'll register every command so
 | the console gets access to each of the command object instances.
 |
-*/
+ */
 
+foreach (glob(app_path() . '/bfacp/Commands/*.php') as $file) {
+    if (preg_match("/([a-zA-Z]+\.php)/", $file, $matches)) {
+        $classname = str_replace('.php', null, $matches[1]);
+        Artisan::resolve(sprintf('BFACP\Commands\%s', $classname));
+    }
+}
