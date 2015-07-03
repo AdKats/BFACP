f9de5557e15cc8eea8a900cbba8c4b989e5af312
Prophet731 <webmaster@amurray.me>
2015-07-02 22:45:15 -0700

    Updated SQL files

--- app/storage/sql/adkats_statistics.sql
+++ app/storage/sql/adkats_statistics.sql
@@ -12,15 +12,6 @@ SELECT
                 FROM
                     `adkats_records_main`
                 WHERE
-                    `command_action` IN (3 , 4, 5, 54)
-                        AND (`source_name` <> 'AutoAdmin')
-                        AND `target_id` IS NOT NULL),
-            3) AS `PercentageKilled_Admins`,
-    ROUND((SELECT DISTINCT
-                    COUNT(`target_id`)
-                FROM
-                    `adkats_records_main`
-                WHERE
                     `command_action` IN (6)
                         AND (`source_name` <> 'AFKManager')
                         AND `target_id` IS NOT NULL),
@@ -30,29 +21,9 @@ SELECT
                 FROM
                     `adkats_records_main`
                 WHERE
-                    `command_action` IN (6)
-                        AND (`source_name` <> 'AFKManager'
-                        AND `source_name` <> 'AutoAdmin')
-                        AND `target_id` IS NOT NULL),
-            3) AS `PercentageKicked_Admins`,
-    ROUND((SELECT DISTINCT
-                    COUNT(`target_id`)
-                FROM
-                    `adkats_records_main`
-                WHERE
                     `command_action` IN (7 , 8, 72, 73)
                         AND `target_id` IS NOT NULL),
             3) AS `PercentageBanned`,
-    ROUND((SELECT DISTINCT
-                    COUNT(`target_id`)
-                FROM
-                    `adkats_records_main`
-                WHERE
-                    `command_action` IN (7 , 8, 72, 73)
-                        AND (`source_name` <> 'BanEnforcer'
-                        AND `source_name` <> 'AutoAdmin')
-                        AND `target_id` IS NOT NULL),
-            3) AS `PercentageBanned_Admins`,
     ROUND((SELECT
                     COUNT(`ban_id`)
                 FROM
--- app/storage/sql/sbRoundStats.sql
+++ app/storage/sql/sbRoundStats.sql
@@ -4,9 +4,9 @@ FROM
     tbl_extendedroundstats
 WHERE
     server_id = ?
-HAVING round_Id = (SELECT
-        MAX(round_id)
-    FROM
-        tbl_extendedroundstats
-    WHERE
-        server_id = ?)
+        AND round_id = (SELECT
+            MAX(round_id)
+        FROM
+            tbl_extendedroundstats
+        WHERE
+            server_id = ?)
