108b3b822dff42c3e1bf0ad55550f34adc156574
Prophet731 <webmaster@amurray.me>
2015-07-02 22:46:58 -0700

    Updated

--- public/js/factorys/ScoreboardAdminFactory.js
+++ public/js/factorys/ScoreboardAdminFactory.js
@@ -1,19 +1,76 @@
 angular.module('bfacp').factory('SBA', ['$http', function($http) {
-    var payload = {};
-    var url = 'api/servers/scoreboard/admin';
+    var baseurl = 'api/servers/admin/scoreboard';
 
     return {
-        say: function(sid, players, type, message, teamID) {
+        say: function(server_id, players, type, message, teamID) {
             payload = {
-                method: 'say',
-                server_id: sid,
+                server_id: server_id,
                 type: type,
                 message: message,
                 team: teamID,
                 players: players
             };
 
-            return $http.post(url, payload);
+            return $http.post(baseurl + '/say', payload);
+        },
+
+        yell: function(server_id, players, type, message, teamID, duration) {
+            payload = {
+                server_id: server_id,
+                type: type,
+                message: message,
+                team: teamID,
+                players: players,
+                duration: duration
+            };
+
+            return $http.post(baseurl + '/yell', payload);
+        },
+
+        tell: function(server_id, players, message) {
+            payload = {
+                server_id: server_id,
+                type: type,
+                message: message,
+                players: players
+            };
+
+            return $http.post(baseurl + '/tell', payload);
+        },
+
+        kill: function(server_id, players, message) {
+            payload = {
+                server_id: server_id,
+                type: type,
+                message: message,
+                players: players
+            };
+
+            return $http.post(baseurl + '/kill', payload);
+        },
+
+        kick: function(server_id, players, message) {
+            payload = {
+                server_id: server_id,
+                type: type,
+                message: message,
+                players: players
+            };
+
+            return $http.post(baseurl + '/kick', payload);
+        },
+
+        teamswitch: function(server_id, players, team, squad, locked) {
+            payload = {
+                server_id: server_id,
+                type: type,
+                team: team,
+                squad: squad,
+                locked: locked,
+                players: players
+            };
+
+            return $http.post(baseurl + '/teamswitch', payload);
         }
     }
 }]);
