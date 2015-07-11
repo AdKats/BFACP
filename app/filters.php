69c0d3323d3e406fe96bf13f6ae87eee871272bc
Prophet731 <webmaster@amurray.me>
2015-07-11 10:21:44 -0700

    Added HTML Minification

--- app/filters.php
+++ app/filters.php
@@ -52,7 +52,19 @@ App::before(function ($request) {
 });
 
 App::after(function ($request, $response) {
-    //
+    // HTML Minification
+    if(App::environment() != 'local') {
+        if($response instanceof Illuminate\Http\Response) {
+            $output = $response->getOriginalContent();
+             // Clean comments
+            $output = preg_replace('/<!--([^\[|(<!)].*)/', '', $output);
+            $output = preg_replace('/(?<!\S)\/\/\s*[^\r\n]*/', '', $output);
+             // Clean Whitespace
+            $output = preg_replace('/\s{2,}/', '', $output);
+            $output = preg_replace('/(\r?\n)/', '', $output);
+            $response->setContent($output);
+        }
+    }
 });
 
 /*
