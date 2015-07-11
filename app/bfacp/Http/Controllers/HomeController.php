e56f3e9a13f1e9b3026b4f431e89867d9717c950
Prophet731 <webmaster@amurray.me>
2015-07-11 01:37:58 -0700

    Updated HomeController.php

--- app/bfacp/Http/Controllers/HomeController.php
+++ app/bfacp/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php namespace BFACP\Http\Controllers;
 
 use BFACP\Battlefield\Game;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
@@ -10,36 +11,26 @@ use Illuminate\Support\Facades\View;
 class HomeController extends BaseController
 {
     /**
-     * Player Repository
-     * @var BFACP\Repositories\PlayerRepository
-     */
-    private $repository;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->repository = \App::make('BFACP\Repositories\PlayerRepository');
-    }
-
-    /**
      * Shows the dashboard
      */
     public function index()
     {
+        $playerRepository = App::make('BFACP\Repositories\PlayerRepository');
+
         // Cache results for 1 day
-        $uniquePlayers = Cache::remember('players.unique.total', 60 * 24, function () {
-            return $this->repository->getPlayerCount();
+        $uniquePlayers = Cache::remember('players.unique.total', 60 * 24, function () use (&$playerRepository) {
+            return $playerRepository->getPlayerCount();
         });
 
         // Cache results for 1 day
-        $adkats_statistics = Cache::remember('adkats.statistics', 60 * 24, function () {
+        $adkats_statistics = Cache::remember('adkats.statistics', 60 * 24, function () use (&$playerRepository) {
             $results = DB::select(File::get(storage_path() . '/sql/adkats_statistics.sql'));
             return head($results);
         });
 
         // Cache results for 1 day
-        $countryMap = Cache::remember('players.seen.country', 60 * 24, function () {
-            return $this->repository->getPlayersSeenByCountry();
+        $countryMap = Cache::remember('players.seen.country', 60 * 24, function () use (&$playerRepository) {
+            return $playerRepository->getPlayersSeenByCountry();
         });
 
         $countryMapTable = $countryMap->sortByDesc('total')->take(5);
