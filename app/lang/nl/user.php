5bbf8e711d93e7707a1db87a3d0d518abd1a06ec
Prophet731 <webmaster@amurray.me>
2015-10-07 14:11:38 -0700

    Copied english language file Needs to be translated

---
+++ app/lang/nl/user.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'account' => [
+        'settings' => [
+            'blocks' => [
+                'general' => [
+                    'title' => 'General',
+                    'inputs' => [
+                        'email' => 'Email',
+                        'language' => 'Language',
+                    ],
+                ],
+                'password' => [
+                    'title' => 'Change Password',
+                    'inputs' => [
+                        'password' => 'Password',
+                        'password_confirm' => 'Confirm Password',
+                    ],
+                    'inputs_help' => [
+                        'password' => 'To change your password please enter it here.',
+                        'password_confirm' => 'Retype your new password to confirm the change.',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'notifications' => [
+        'account' => [
+            'email' => [
+                'changed' => 'Email has been updated to :addr!',
+            ],
+            'password' => [
+                'changed' => 'Password has been changed!',
+            ],
+            'language' => [
+                'changed' => 'Language has been changed to :lang!',
+            ],
+        ],
+    ],
+];
