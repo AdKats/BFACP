edf903c7597bfc5473404bb7edda93174b6b9548
Prophet731 <webmaster@amurray.me>
2015-12-21 22:44:49 -0800

    Revert "Updated adkats_statistics.sql"
    This reverts commit 1bedb9424639cb46f43a8477cab1607e79b2131b.

--- app/storage/sql/adkats_statistics.sql
+++ app/storage/sql/adkats_statistics.sql
@@ -1,37 +1,21 @@
-SELECT 
-    ROUND((SELECT DISTINCT
-                    COUNT(`target_id`)
-                FROM
-                    `adkats_records_main`
-                WHERE
-                    `command_action` IN (3 , 4, 5, 54)
-                        AND `target_id` IS NOT NULL
-                        AND `target_id` != `source_id`),
-            3) AS `PercentageKilled`,
-    ROUND((SELECT DISTINCT
-                    COUNT(`target_id`)
-                FROM
-                    `adkats_records_main`
-                WHERE
-                    `command_action` IN (6)
-                        AND (`source_name` <> 'AFKManager')
-                        AND `target_id` IS NOT NULL
-                        AND `target_id` != `source_id`),
-            3) AS `PercentageKicked`,
-    ROUND((SELECT DISTINCT
-                    COUNT(`target_id`)
-                FROM
-                    `adkats_records_main`
-                WHERE
-                    `command_action` IN (7 , 8)
-                        AND `target_id` IS NOT NULL
-                        AND `target_id` != `source_id`),
-            3) AS `PercentageBanned`,
-    ROUND((SELECT 
-                    COUNT(`ban_id`)
-                FROM
-                    `adkats_bans`
-                WHERE
-                    `ban_status` = 'Active'
-                        AND `ban_endTime` > UTC_TIMESTAMP()),
-            3) AS `PercentageBanned_Active`
+SELECT ROUND(
+               (SELECT DISTINCT COUNT(`target_id`)
+                FROM `adkats_records_main`
+                WHERE `command_action` IN (3 , 4, 5, 54)
+                  AND `target_id` IS NOT NULL), 3) AS `PercentageKilled`,
+       ROUND(
+               (SELECT DISTINCT COUNT(`target_id`)
+                FROM `adkats_records_main`
+                WHERE `command_action` IN (6)
+                  AND (`source_name` <> 'AFKManager')
+                  AND `target_id` IS NOT NULL), 3) AS `PercentageKicked`,
+       ROUND(
+               (SELECT DISTINCT COUNT(`target_id`)
+                FROM `adkats_records_main`
+                WHERE `command_action` IN (7 , 8, 72, 73)
+                  AND `target_id` IS NOT NULL), 3) AS `PercentageBanned`,
+       ROUND(
+               (SELECT COUNT(`ban_id`)
+                FROM `adkats_bans`
+                WHERE `ban_status` = 'Active'
+                  AND `ban_endTime` > UTC_TIMESTAMP()), 3) AS `PercentageBanned_Active`
