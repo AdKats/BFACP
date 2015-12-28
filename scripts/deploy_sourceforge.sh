38e33143b6619fd950c18f41ec98666feac17ce2
Prophet731 <webmaster@amurray.me>
2015-12-28 09:23:31 -0800

    Update circle.yml

--- scripts/deploy_sourceforge.sh
+++ scripts/deploy_sourceforge.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -eu
-
-echo ${CIRCLE_BRANCH}
+parentdir="$(dirname "$(pwd)")"
+#FILENAME=${CIRCLE_BRANCH}
+USER="prophet731"
+HOST="frs.sourceforge.net"
+scp -oUserKnownHostsFile=/dev/null "${parentdir}/README.md" $USER@$HOST:/home/pfs/project/b/bf/bfacp/
