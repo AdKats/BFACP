39b6aacd2100de7d30f2b9115a8e1971ee9eedc0
Prophet731 <webmaster@amurray.me>
2016-01-10 19:37:42 -0800

    Updated

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,15 +1,54 @@
 <?php
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseTest extends TestCase
 {
     public function testPrerequisiteTables()
     {
-        $sql = File::get(storage_path('sql/tests/PrerequisiteTables.sql'));
-        DB::unprepared($sql);
+        Schema::create('tbl_games', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->tinyInteger('GameID', true, true);
+            $table->string('Name', 45)->nullable()->default(null);
+        });
+
+        Schema::create('tbl_playerdata', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('PlayerID');
+            $table->tinyInteger('GameID', false, true);
+            $table->string('ClanTag', 10)->nullable()->default(null);
+            $table->string('SoldierName', 30)->nullable()->default(null)->index();
+            $table->smallInteger('GlobalRank')->default(0);
+            $table->string('PBGUID', 32)->nullable()->default(null)->index();
+            $table->string('EAGUID', 35)->nullable()->default(null)->index();
+            $table->string('IP_Address', 15)->nullable()->default(null)->index();
+            $table->string('CountryCode', 2)->nullable()->default(null)->index();
+            $table->unique(['EAGUID', 'GameID'], 'UNIQUE_playerdata');
+            $table->foreign('GameID')->references('GameId')->on('tbl_games');
+        });
+
+        Schema::create('tbl_server', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->smallIncrements('ServerID');
+            $table->tinyInteger('ServerGroup', false, true)->default(0)->index();
+            $table->string('IP_Address', 45)->nullable()->default(null);
+            $table->string('ServerName', 200)->nullable()->default(null);
+            $table->tinyInteger('GameID', false, true);
+            $table->smallInteger('usedSlots', false, true)->default(0);
+            $table->smallInteger('maxSlots', false, true)->default(0);
+            $table->string('mapName', 45)->nullable()->default(null);
+            $table->text('fullMapName');
+            $table->string('Gamemode', 45)->nullable()->default(null);
+            $table->string('GameMod', 45)->nullable()->default(null);
+            $table->string('PBversion', 45)->nullable()->default(null);
+            $table->string('ConnectionState', 45)->nullable()->default(null);
+            $table->unique('IP_Address');
+        });
+
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
     }
 
     /**
@@ -17,8 +56,6 @@ class DatabaseTest extends TestCase
      */
     public function testDatabase()
     {
-        Artisan::call('migrate:refresh');
-        Artisan::call('db:seed');
         $this->seeInDatabase('bfacp_roles', ['name' => 'Administrator']);
         $this->seeInDatabase('bfacp_roles', ['name' => 'Registered']);
         $this->seeInDatabase('bfacp_users', ['username' => 'Admin']);
