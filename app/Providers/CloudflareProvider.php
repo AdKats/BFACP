1fc1f6ce724dbb9ace122be1b890085a35a50a0b
Prophet731 <webmaster@amurray.me>
2016-01-15 21:56:44 -0800

    Created cloudflare provider

---
+++ app/Providers/CloudflareProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace BFACP\Providers;
+
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+
+class CloudflareProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $request = $this->app['request'];
+        $cache = $this->app['cache'];
+        try {
+            $proxies = $cache->remember('cloudflare.ips', 24 * 60 * 7, function () use (&$request) {
+                $guzzle = app('Guzzle');
+                $ipv4 = $guzzle->get('https://www.cloudflare.com/ips-v4');
+                $ipv6 = $guzzle->get('https://www.cloudflare.com/ips-v6');
+                $ipv4 = array_filter(explode("\n", $ipv4->getBody()));
+                Log::info('Cloudflare IPv4 Loaded', [
+                    'ips' => $ipv4
+                ]);
+
+                $ipv6 = array_filter(explode("\n", $ipv6->getBody()));
+                Log::info('Cloudflare IPv6 Loaded', [
+                    'ips' => $ipv6
+                ]);
+
+                return array_merge($ipv4, $ipv6);
+            });
+
+            $request->setTrustedProxies($proxies);
+        } catch (Exception $e) {
+            $cache->forget('cloudflare.ips');
+            Log::warning('Unable to setup cloudflare trusted proxies.', [
+                'exception' => $e->getMessage()
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
