44302e1fce531aecfb3cde04b79b25f1be803b98
Prophet731 <webmaster@amurray.me>
2016-01-23 22:19:27 -0800

    Population History SQL

---
+++ storage/sql/populationHistory.sql
@@ -0,0 +1,13 @@
+SELECT
+  DATE(c.calendar_datetime)          AS GameDate,
+  HOUR(c.calendar_datetime)          AS hour_of_day,
+  IFNULL(FORMAT(m.AvgPlayers, 0), 0) AS 'PlayerAvg'
+FROM
+  bfacp_calendar c
+  LEFT JOIN
+  tbl_mapstats m ON (DATE(c.calendar_datetime) = DATE(m.TimeRoundEnd)
+                     AND HOUR(c.calendar_datetime) = HOUR(m.TimeRoundEnd))
+                    AND m.ServerID = ?
+WHERE
+  (c.calendar_datetime BETWEEN DATE(DATE_SUB(UTC_TIMESTAMP, INTERVAL 2 WEEK)) AND DATE(UTC_TIMESTAMP()))
+GROUP BY GameDate, hour_of_day
