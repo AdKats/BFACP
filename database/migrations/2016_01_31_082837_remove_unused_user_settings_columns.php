7befeae270136b54128117a3b812cbfd62a095a7
Prophet731 <webmaster@amurray.me>
2016-01-31 00:40:36 -0800

    Added migration to remove unused columns

---
+++ database/migrations/2016_01_31_082837_remove_unused_user_settings_columns.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveUnusedUserSettingsColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bfacp_settings_users', function ($table) {
+            $table->dropColumn('notifications_alert_sound');
+            $table->dropColumn('notifications_alert');
+            $table->dropColumn('timezone');
+            $table->dropIndex('bfacp_settings_users_timezone_index');
+        });
+    }
+}
