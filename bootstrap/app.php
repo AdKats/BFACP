6737175c323d58b3ec3546ad5edf2e995524c348
Prophet731 <webmaster@amurray.me>
2016-02-07 00:15:14 -0500

    Applied fixes from StyleCI

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -47,18 +47,18 @@ $app->singleton(
 
 $app->singleton('Guzzle', GuzzleHttp\Client::class);
 
-if (!$app->runningInConsole()) {
+if (! $app->runningInConsole()) {
     $setupFilePath = app_path('setup.php');
     $jsBuildDirectoryPath = public_path('js/builds');
     $minPHPVersion = '5.5.9';
 
-    if (version_compare(phpversion(), $minPHPVersion, '<') || !extension_loaded('mcrypt') || !extension_loaded('pdo')) {
+    if (version_compare(phpversion(), $minPHPVersion, '<') || ! extension_loaded('mcrypt') || ! extension_loaded('pdo')) {
         die(view('system.requirements', ['required_php_version' => $minPHPVersion]));
     }
 
     if (file_exists($setupFilePath) && $app->environment() == 'production') {
         require_once $setupFilePath;
-        if (!unlink($setupFilePath)) {
+        if (! unlink($setupFilePath)) {
             die(sprintf('Please delete installer located at "%s"', $setupFilePath));
         }
     }
