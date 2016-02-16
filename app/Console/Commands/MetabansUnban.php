b91c118a6b7b3fdd352ef785e511c9738801b719
Prophet731 <webmaster@amurray.me>
2016-02-15 19:13:32 -0800

    Updated metabans command

--- app/Console/Commands/MetabansUnban.php
+++ app/Console/Commands/MetabansUnban.php
@@ -4,9 +4,10 @@ namespace BFACP\Console\Commands;
 
 use BFACP\Battlefield\Player;
 use BFACP\Exceptions\MetabansException;
+use BFACP\Libraries\Metabans;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\App;
+use InvalidArgumentException;
 
 /**
  * Class MetabansUnban.
@@ -28,10 +29,10 @@ class MetabansUnban extends Command
     protected $description = 'Allows you to remove a ban from metabans.';
 
     private $questions = [
-        'Q1' => 'What is the ID of the player you wish to unban? ',
-        'Q2' => 'Is this the correct player? [%s] %s. [yes|no] ',
-        'Q3' => 'What is the reason for unbanning? (Leave blank to use default): ',
-        'Q4' => '%s will be unbanned from metabans with the reason of "%s". Continue? [yes|no] ',
+        'Q1' => 'What is the ID of the player you wish to unban?',
+        'Q2' => 'Is this the correct player? [%s] %s.',
+        'Q3' => 'What is the reason for unbanning? (Leave blank to use default):',
+        'Q4' => '%s will be unbanned from metabans with the reason of "%s". Continue?',
     ];
 
     /**
@@ -47,22 +48,16 @@ class MetabansUnban extends Command
      *
      * @return mixed
      */
-    public function fire()
+    public function handle()
     {
         $_playerFound = false;
 
-        try {
-            $metabans = App::make('BFACP\Libraries\Metabans');
-        } catch (MetabansException $e) {
-            $this->error('Failed to initialize the Metabans Library.');
-            die;
-        }
-
         do {
+            $metabans = new Metabans();
             $playerID = $this->ask($this->questions['Q1']);
             try {
                 if (! is_numeric($playerID)) {
-                    throw new \InvalidArgumentException();
+                    throw new InvalidArgumentException();
                 }
 
                 $player = Player::findOrFail($playerID);
@@ -73,11 +68,7 @@ class MetabansUnban extends Command
                     $_playerFound = true;
 
                     if (! is_null($metabans)) {
-                        $unbanReason = $this->ask($this->questions['Q3']);
-
-                        if (empty($unbanReason)) {
-                            $unbanReason = 'Unbanned';
-                        }
+                        $unbanReason = $this->ask($this->questions['Q3'], 'Unbanned');
 
                         $question2 = sprintf($this->questions['Q4'], $player->SoldierName, $unbanReason);
 
@@ -92,10 +83,10 @@ class MetabansUnban extends Command
                 }
             } catch (ModelNotFoundException $e) {
                 $this->error(sprintf('Could not find the player with the ID "%s". Please try again.', $playerID));
-            } catch (\InvalidArgumentException $e) {
+            } catch (InvalidArgumentException $e) {
                 $this->error(sprintf('Only integers are allowed for the player id. Input was: %s', gettype($playerID)));
             } catch (MetabansException $e) {
-                $this->error('Failed to initialize the Metabans Library.');
+                $this->error($e->getMessage());
                 die;
             }
         } while ($_playerFound == false);
