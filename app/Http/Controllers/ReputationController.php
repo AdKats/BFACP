2d9f0dbd9db5239154fb376f66122c97aa82011c
Prophet <webmaster@amurray.me>
2016-05-18 11:59:42 +0000

    Created reputation controller

---
+++ app/Http/Controllers/ReputationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BFACP\Http\Controllers;
+
+use BFACP\Battlefield\Reputation;
+
+/**
+ * Class ReputationController
+ */
+class ReputationController extends Controller
+{
+    /**
+     * Shows the reputation listing
+     */
+    public function index()
+    {
+        $rep = Reputation::paginate(30);
+
+        return $rep;
+    }
+}
