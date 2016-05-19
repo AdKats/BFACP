3384ce25d0fe01aa791049eac3e33eadebf8cb37
Prophet731 <webmaster@amurray.me>
2016-05-19 03:06:52 -0700

    Updates to unit testing files

--- scripts/deploy_sourceforge.sh
+++ scripts/deploy_sourceforge.sh
@@ -8,6 +8,43 @@ elif [[ -v CIRCLE_TAG ]]; then
 else
     exit 1
 fi
+
+cat > .env << EOF
+APP_KEY=VIskgutkOS1aLQBOiIxxYYyJpaTiTMxb
+
+DB_HOST=127.0.0.1
+DB_DATABASE=circle_test
+DB_USERNAME=ubuntu
+DB_PASSWORD=
+APP_ENV=testing
+CACHE_DRIVER=array
+SESSION_DRIVER=array
+QUEUE_DRIVER=sync
+MAIL_DRIVER=log
+
+# Add your IPv4 or IPv6 address here to show more detailed debug
+# information and also view the system maintence page.
+# You can add more than one ip address here if you need too.
+# Just seprate each address with the pipe character (|).
+#
+# Example:
+#
+# 127.0.0.1|10.0.0.1|172.0.0.1
+#
+# By default it allows only the localhost
+IP_WHITELIST=127.0.0.1
+
+# Set your pusher API ID, Key, and Secret here.
+# You will need an account at https://pusher.com
+PUSHER_APP_ID=null
+PUSHER_KEY=null
+PUSHER_SECRET=null
+EOF
+
+/home/ubuntu/.phpenv/shims/php artisan optimize
+
+rm -f .env
+
 echo "Zipping application into ${FILENAME}."
 zip -9 -q -r $FILENAME . -x *.git*
 USER="prophet731"
