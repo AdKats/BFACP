7595c93a6080e9af7f11aeccbf5b336884e2c75b
Nexon <nexonrievsky@gmail.com>
2016-07-10 14:30:41 +0500

    Russian lang files update
    Localization for lar5 + some fixes

--- resources/lang/ru/alerts.php
+++ resources/lang/ru/alerts.php
@@ -3,7 +3,7 @@
 return [
 
     'user' => [
-        'soldier_taken' => 'Игрок #:playerid уже используется другим игроком.',
+        'soldier_taken' => 'Игрок #:playerid уже используется другим пользователем.',
         'invalid'       => 'Пользователь #:userid не существует.',
         'deleted'       => ':username был удален.',
         'saved'         => 'Профиль был обновлен!',
